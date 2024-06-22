7607107fcb0d671900984d370ee79ac2e04ed29f
Markus Dam <markus.dam123@gmail.com>
2024-06-22 18:31:57 +0200

    configure cpu

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -3,5 +3,6 @@
   imports = [
     ./hardware-configuration.nix
     ./monitors.nix
+    ./gpu.nix
   ];
 }
---
+++ hosts/desktop/gpu.nix
@@ -0,0 +1,11 @@
+{ config, ... }@inputs:
+{
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
