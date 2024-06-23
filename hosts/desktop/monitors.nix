fe0ccbdcfe24c8eb063024081ca0f267cd458216
Markus Dam <markus.dam123@gmail.com>
2024-06-23 23:56:57 +0200

    fix monitors on desktop

--- hosts/desktop/monitors.nix
+++ hosts/desktop/monitors.nix
@@ -1,6 +1,6 @@
 { ... }@inputs:
 {
   services.xserver.displayManager.sessionCommands = ''
-    xrandr --output DP-1 --primary --mode 1920x1080 --rate 144 --output HDMI-1 --mode 1920x1080 --left-of DP-1
+    xrandr --output DP-0 --primary --mode 1920x1080 --rate 144 --output HDMI-0 --mode 1920x1080 --left-of DP-0
   '';
 }
