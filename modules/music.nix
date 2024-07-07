cda43cc5330a371f630f9487efa5a905e1bdeddb
Markus Dam <markus.dam123@gmail.com>
2024-07-07 18:25:50 +0200

    fix mopidy config

--- modules/music.nix
+++ modules/music.nix
@@ -8,6 +8,30 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    sops = {
+      secrets = {
+        "subsonic_url" = {};
+        "subsonic_username" = {};
+        "subsonic_password" = {};
+        "scrobbler_username" = {};
+        "scrobbler_password" = {};
+      };
+
+      templates."mopidy.conf" = {
+        content = ''
+          [scrobbler]
+          password = ${config.sops.placeholder."scrobbler_password"}
+          username = ${config.sops.placeholder."scrobbler_username"}
+
+          [subidy]
+          password = ${config.sops.placeholder."subsonic_password"}
+          url = ${config.sops.placeholder."subsonic_url"}
+          username = ${config.sops.placeholder."subsonic_username"}
+        '';
+        mode = "0440";
+        owner = config.users.users.mopidy.name;
+      };
+    };
     services.mopidy = {
       enable = cfg.enable;
       extensionPackages = with pkgs; [
@@ -15,10 +39,6 @@ in
         mopidy-subidy
         mopidy-scrobbler
       ];
-      configuration = ''
-        [mpd]
-        enabled = true;
-      '';
       extraConfigFiles = [ config.sops.templates."mopidy.conf".path ];
     };
 
