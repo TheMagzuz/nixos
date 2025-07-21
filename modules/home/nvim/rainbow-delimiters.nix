b021f8279e84f7820cd7add09417a671c297dae5
magz <11150183+TheMagzuz@users.noreply.github.com>
2025-07-21 22:09:40 +0200

    make rainbow-delimiters track main instead of nixpkgs
    the main branch has a fix for an issue with delimiters not working in
    rust, that isn't packaged yet

--- modules/home/nvim/rainbow-delimiters.nix
+++ modules/home/nvim/rainbow-delimiters.nix
@@ -1,5 +1,25 @@
-_: {
-  programs.nvf.settings.vim.visuals.rainbow-delimiters = {
-    enable = true;
+{pkgs, ...}: {
+  # programs.nvf.settings.vim.visuals.rainbow-delimiters = {
+  #   enable = true;
+  # };
+  programs.nvf.settings.vim.extraPlugins = {
+    rainbow-delimiters = {
+      package = pkgs.vimUtils.buildVimPlugin {
+        pname = "rainbow-delimiters";
+        version = "0.9.1";
+        src = pkgs.fetchFromGitHub {
+          owner = "HiPhish";
+          repo = "rainbow-delimiters.nvim";
+          rev = "49372aadaaf04d14a50efaa34150c51d5a8047e1";
+          hash = "sha256-qvYpFcqLJ/DCdgGUaeaEOna9J9Rcsnj98OQr1ioINiI=";
+        };
+        nvimSkipModules = [
+          # rainbow-delimiters.types.lua
+          "rainbow-delimiters.types"
+          # Test that requires an unpackaged dependency
+          "rainbow-delimiters._test.highlight"
+        ];
+      };
+    };
   };
 }
