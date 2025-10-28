4572858ae64b20c9d22077f7feeaf636897fea2d
magz <11150183+TheMagzuz@users.noreply.github.com>
2025-10-28 15:03:37 +0100

    add podman module

---
+++ modules/podman.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}: {
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+  environment.systemPackages = with pkgs; [
+    podman-compose
+  ];
+}
